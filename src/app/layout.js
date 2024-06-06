ebd10e9ca586b6b6ee38be50c76512d490a972ac
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-06-06 19:58:46 +0600

    News component updated.

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,15 @@ export default function RootLayout({ children }) {
                         <Sidebar />
                     </div>
                     <div>{children}</div>
-                    <div>
-                        <News />
+                    <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+                        <div className="sticky top-0 bg-white py-2">
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+                            />
+                            <News />
+                        </div>
                     </div>
                 </div>
             </body>
