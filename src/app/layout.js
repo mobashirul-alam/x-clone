dbe05190dbe0a4bf61939c6b1ca66d3d57945f6d
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-06-06 09:13:15 +0600

    Sidebar updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body className={inter.className}>
                 <div className="flex justify-between max-w-7xl mx-auto">
-                    <div>
+                    <div className="hidden sm:inline border-r h-screen">
                         <Sidebar />
                     </div>
                     <div>{children}</div>
