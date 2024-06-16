0c9008ce6e62a542b9728f76037bdd72de412a1f
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-06-16 22:32:24 +0600

    Single post page updated and showing post details.

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,5 +1,25 @@
-const PostPage = () => {
-    return <div>PostPage</div>;
+import Post from "@/components/Post";
+import { app } from "@/firebase.init";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import Link from "next/link";
+import { HiArrowLeft } from "react-icons/hi";
+
+const PostPage = async ({ params }) => {
+    const db = getFirestore(app);
+    let data = {};
+    const querySnapshot = await getDoc(doc(db, "posts", params.id));
+    data = { ...querySnapshot.data(), id: querySnapshot.id };
+    return (
+        <div className="max-w-xl mx-auto border-r border-l min-h-screen">
+            <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+                <Link href={"/"} className="hover:bg-gray-100 rounded-full p-2">
+                    <HiArrowLeft className="h-5 w-5" />
+                </Link>
+                <h2 className="sm:text-lg">Back</h2>
+            </div>
+            <Post post={data} id={data.id} />
+        </div>
+    );
 };
 
 export default PostPage;
